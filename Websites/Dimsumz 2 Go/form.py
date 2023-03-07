5f7a00f7463d783c09ed6a60654a9fb9a03181fd
Alex M <noreply>
2023-03-07 17:22:56 +0800

    Python file

--- Websites/Dimsumz 2 Go/form.py
+++ Websites/Dimsumz 2 Go/form.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
+from wtforms import StringField, SelectField, PasswordField, URLField, SelectField
 from wtforms.validators import DataRequired, Length, EqualTo
+from flask_ckeditor import CKEditorField
 
 
 class LoginForm(FlaskForm):
@@ -26,3 +27,18 @@ class RegistrationForm(FlaskForm):
 
     lastName = StringField(label='Last name', validators=[
                            DataRequired(), Length(min=2, max=30)])
+
+
+class CreateForm(FlaskForm):
+
+    recipe = StringField(label='Recipe', validators=[
+                         DataRequired(), Length(min=3, max=32)])
+    desc = StringField(label="Title Description", validators=[
+                       DataRequired(), Length(min=10, max=138)])
+    url = URLField(label='URL of the image', validators=[
+                   DataRequired(), Length(min=20, max=250)])
+    ingredients = SelectField(
+        label="Select an Ingredient", validators=[DataRequired()])
+
+    instructions = CKEditorField(
+        'Instructions of Recipe', validators=[DataRequired()])
--- Websites/Dimsumz 2 Go/main.py
+++ Websites/Dimsumz 2 Go/main.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
 from icecream import ic
 from flask_hashing import Hashing
-from form import LoginForm, RegistrationForm
+from form import LoginForm, RegistrationForm, CreateForm
 from time import sleep
 from functools import wraps
 
@@ -11,7 +11,7 @@ from functools import wraps
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///dimsumz2go.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'your secret key'
+app.config['SECRET_KEY'] = 'your secret key hehelol'
 login_manager = LoginManager()
 login_manager.init_app(app)
 hashing = Hashing(app)
@@ -23,18 +23,45 @@ class Users(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
 
-    employee_id = (db.Integer, db.ForeignKey('employee.id'))
+    employee_id = (db.Integer, db.ForeignKey('employees.id'))
     # username is the primary key constraint and of course id is the primary key
     username = db.Column(db.String(250), unique=True, nullable=False)
     password = db.Column(db.String(450), unique=False, nullable=False)
+    access_type_id = (db.Integer, db.ForeignKey('access_type.id'))
 
 
 class Employees(UserMixin, db.Model):
 
-    __tablename__ = 'employee'
+    __tablename__ = 'employees'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     firstName = db.Column(db.String(250), unique=False, nullable=False)
     lastName = db.Column(db.String(250), unique=False, nullable=False)
+
+
+class accessType(UserMixin, db.Model):
+    __tablename__ = 'access_type'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    role = db.Column(db.String(100), unique=False, nullable=False)
+
+
+class menuItem(db.Model):
+
+    __tablename__ = 'menu_item'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(32), unique=False, nullable=False)
+    desc = db.Column(db.String(138), unique=False, nullable=False)
+    cost = db.Column(db.Float(20), nullable=False)
+
+
+class Image(db.Model):
+
+    __tablename__ = 'image'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    menu_item_id = db.Column(db.Integer, db.ForeignKey(
+        'menu_item.id'), nullable=False)
+    path = db.Column(db.String(250), nullable=False)
 
 
 db.create_all()
@@ -46,6 +73,7 @@ def check_user(func):
         current_url = request.path  # /graphs
 
         id_and_url = (user_type, current_url)
+        ic(current_url)
 
         categories = [(1, "/recipes"), (2, "/graphs"), (2, '/inventory'), (3, '/recipes'),
                       (3, '/inventory'), (4, '/recipes'), (4, '/graphs'), (4, '/inventory')]
@@ -151,7 +179,7 @@ def logout():
 @app.route("/unauthorized", methods=['GET', 'POST'])
 def unauthorized():
 
-    return "<h1>Unauthorized, you don't have enough permission to access this page.</h1>"
+    return "<h1>Unauthorized, you don't have enough permission to access this page.</h1> <br> <a href={{ url_for('dashboard') }}>Back to dashboard.</a>"
 
 
 @app.route('/graphs', methods=['GET', 'POST'])
@@ -174,16 +202,39 @@ def inventory():
 @check_user
 def recipes():
 
-    return render_template("recipes.html")
+    menus = menuItem.query.all()
+    imgs = Image.query.all()
+
+    return render_template("recipes.html", menu=menus, img=imgs, zip=zip)
 
 
-@ app.route('/home', methods=['GET', 'POST'])
+@app.route('/item/<int:num>', methods=['GET', 'POST'])
+@login_required
+# @check_user TODO find a way to implement this:
+def item(num):
+
+    img = Image.query.filter_by(id=num).first()
+
+    return f"{num} {img}"
+
+
+@app.route('/home', methods=['GET', 'POST'])
 def home():
 
     return "<h1>Simple static website for users</h1>"
 
 
-@ login_manager.user_loader
+@app.route('/create', methods=['GET', 'POST'])
+@login_required
+@check_user
+def create():
+
+    form = CreateForm
+
+    return render_template('create.html', form=form)
+
+
+@login_manager.user_loader
 def load_user(user_id):
 
     return Users.query.get(int(user_id))
